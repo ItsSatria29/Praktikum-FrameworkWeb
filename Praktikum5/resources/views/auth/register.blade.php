335e584c481f19493a4756411d870ea83dbb3754
“ItsSatria29” <satriapanca29@gmail.com>
2025-10-13 12:19:59 +0700

    Registrasi, logout

---
+++ Praktikum5/resources/views/auth/register.blade.php
@@ -0,0 +1,35 @@
+<form method="POST" action="register">
+    @csrf
+    <div>
+        <label for="name">Name</label>
+        <input id="name" type="text" name="name" value="{{ old('name') }}" required autofocus>
+        @error('name')
+            <div>{{ $message }}</div>
+        @enderror
+    </div>
+
+    <div>
+        <label for="email">Email</label>
+        <input id="email" type="email" name="email" value="{{ old('email') }}" required>
+        @error('email')
+            <div>{{ $message }}</div>
+        @enderror
+    </div>
+
+    <div>
+        <label for="password">Password</label>
+        <input id="password" type="password" name="password" required>
+        @error('password')
+            <div>{{ $message }}</div>
+        @enderror
+    </div>
+
+    <div>
+        <label for="password_confirmation">Confirm Password</label>
+        <input id="password_confirmation" type="password" name="password_confirmation" required>
+    </div>
+
+    <div>
+        <button type="submit">Register akun</button>
+    </div>
+</form>
